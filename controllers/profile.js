e09b4b3f41e06c12ad53ead47c2d9c131aad03a4
Nick Monks <nicolasmg94@gmail.com>
2020-09-29 14:42:04 +0100

    First Release - Ready to deploy in Heroku

---
+++ controllers/profile.js
@@ -0,0 +1,22 @@
+const handleProfile = (req,res, db, bcrypt)=>{
+				
+	const { id } = req.params;
+	let found = false;
+
+	db.select('*').from('users').where({
+		id: id
+	})
+	.then(user=>{
+		if (user.length){
+		res.json(user[0])
+	} else {
+		res.status(400).json('Not found')
+	}
+	})
+	.catch(err=> res.status(400).json('Not found'));
+
+}
+
+module.exports =  {
+	handleProfile: handleProfile
+}
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const app = express();
+const knex = require('knex');
+
+// Endpoints
+const register  = require('./controllers/register');
+const signin    = require('./controllers/signin');
+const profile   = require('./controllers/profile');
+const image     = require('./controllers/image');
+
+// Middleware to parse the req.body.email
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+  	host: '127.0.0.1',
+  	user: 'postgres',
+  	password: 'YZcCf3tm?!',
+  	database: 'smart-brain'
+  }
+});
+
+
+app.get('/',            (req, res) => { res.send(database.users)})
+app.post('/signin',     (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register',   (req, res) => { register.handleRegister(req,res,db,bcrypt)})
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req,res,db,bcrypt)})
+app.put('/image',       (req, res) => { image.handleImage(req,res,db,bcrypt)})
+app.post('/imageurl',   (req, res) => { image.handleApiCall(req,res)})
+
+
+app.listen(3000, ()=>{
+	console.log('app is running on port 3000');
+})
+
+
